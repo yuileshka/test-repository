5e08dcc28cfb0a6e6b3ee62078ec89ee5f938d8a
yuileshka <konina.julia3434@gmail.com>
2024-12-19 23:36:45 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Конина Юлия Викторовна";
+  const greeting = "плюс вайб";
 
   console.log('This is App ');
   return (
